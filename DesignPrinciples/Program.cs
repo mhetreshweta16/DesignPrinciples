1789459a2a104d19d2ac15f0d5682d5c8a0b4bf8
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-19 16:08:06 +0530

    [shweta] added the hello message

---
+++ DesignPrinciples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
